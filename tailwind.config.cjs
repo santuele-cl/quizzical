e79b0748caeef188117c14b2a129c92304a51bb2
santuele.cl <santuele.cl@gmail.com>
2023-02-25 18:11:21 +0800

    Updated the UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: '#D6DBF5',
+        lightRed: '#F8BCBC',
+        lightGreen: '#94D7A2', 
+        ashGray: '#F5F7FB',
+        defaultIndigo: '#4D5B9E',
+        darkIndigo: '#293264'
+      }
+    },
   },
   plugins: [],
 }
